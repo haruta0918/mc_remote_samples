aede2426dd3f1c702c032d1faeef949a11792d12
haruta0918 <176218758+haruta0918@users.noreply.github.com>
2025-05-11 09:04:39 +0900

    New

---
+++ examples/demo_03 _minecraft_v.py
@@ -0,0 +1,277 @@
+# demo for 7-segment simulation
+# using the class 'Seven_seg' in seven_seg_pg.py
+
+import sys
+from datetime import datetime
+import pygame
+
+import pygame
+import pygame.freetype
+# import time
+from lcd_font_pg_seven_seg import LCD_font
+from mc_remote.minecraft import Minecraft
+import param_mc_remote as param
+from param_mc_remote import PLAYER_ORIGIN as po
+from param_mc_remote import block
+from param_mc_remote import particle
+from param_mc_remote import entity
+with open("fonts/font_sevnseg.txt", encoding="utf-8") as f:
+        LCD_font_styles = f.read().split('\n')
+
+DARK_GRAY = (40, 40, 40)
+GRAY = (80, 80, 80)
+RED = (255, 0, 0)
+GREEN = (10, 250, 10)
+CYAN = (120, 120, 250)
+YELLOW = (250, 250, 20)
+WHITE = (250, 250, 250)
+
+        
+
+pygame.init()
+
+clock = pygame.time.Clock()
+FPS=1
+screen = pygame.display.set_mode([900, 500])
+pygame.display.set_caption("pygame 7-segment display simulation")
+screen.fill(DARK_GRAY)
+
+
+display1 = LCD_font(screen)
+display1.init_col(BLOCK_SIZE=5, BLOCK_INTV=6, COLOR_ON=GREEN, COLOR_OFF=DARK_GRAY)
+display1.init_row(X_ORG=8, Y_ORG=35, COL_INTV=6)
+dt_now = datetime.now()
+display2 = LCD_font(screen)
+display2.init_col(BLOCK_SIZE=5, BLOCK_INTV=6, COLOR_ON=GREEN, COLOR_OFF=DARK_GRAY)
+display2.init_row(X_ORG=8, Y_ORG=20, COL_INTV=6)
+dt_now = datetime.now()
+mc = Minecraft.create(address=param.ADRS_MCR, port=param.PORT_MCR)
+result = mc.setPlayer(param.PLAYER_NAME, po.x, po.y, po.z)
+if ("Error" in result):
+    sys.exit(result)
+else:
+    print(result)
+    mc.postToChat('kadai #8  時計(完成)')
+lcd1 = LCD_font(mc)
+def LCD_display(x):
+    code = int((x / 8) % 9)
+    lcd1.update_col(col=1, code=code)
+Z=-24
+Y=148
+for y in range(9):
+            Y-=1
+            Z=-24
+            for x in range(49):
+                Z+=1
+                mc.setBlock(48, Y,Z,block.SEA_LANTERN) 
+
+Z=-26
+Y=150
+for y in range(13):
+            Y-=1
+            Z=-26
+            for x in range(53):
+                Z+=1
+                mc.setBlock(47, Y,Z,block.DIAMOND_BLOCK)   
+Z=-25
+Y=148
+for y in range(9):
+    Y-=1
+    Z=-24
+    for x in range(49):
+        Z+=1
+        mc.setBlock(47, Y,Z,block.AIR) 
+    
+# infinite loop top ----
+running = True
+while running:
+    for count in range(16 ** 4):  # 0から65535まで
+        # press ctrl-c or close the window to stop
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+           
+        if not running:
+            break
+        # 「for count」のループから抜ける。whileループも抜ける。
+    # LCD sim
+        code=dt_now.second
+
+        display1.update_col(col=0, code=dt_now.hour // 10)
+        display1.update_col(col=1, code=dt_now.hour % 10)
+        display1.update_col(col=2, code=10)
+        display1.update_col(col=3, code=dt_now.minute // 10)
+        display1.update_col(col=4, code=dt_now.minute % 10)
+        display1.update_col(col=5, code=10)
+        display1.update_col(col=6, code=dt_now.second // 10)
+        display1.update_col(col=7, code=dt_now.second % 10)
+        Z=-24
+        Y=148
+        BLOCKOFF=block.AIR
+        BLOCKON=block.GOLD_BLOCK
+        BLOCK_INTV=1
+        Y=146
+        Z=19
+        print(code)
+        # 秒
+        code=dt_now.second % 10
+        Z=19
+        Y=147
+        for y in range(7):
+            Y-=1
+            Z=19
+            for x in range(5):
+                Z+=1
+                if LCD_font_styles[int(code*7+y)][x] == "1":
+                    BLOCK= BLOCKON
+                    mc.setBlock(47, Y,Z,BLOCK)   
+                else:
+                    BLOCK= BLOCKOFF
+                    mc.setBlock(47, Y,Z,BLOCK)
+                # 桁の原点
+                # ドットの原点座標
+                
+                # ドットを描く
+        code=dt_now.second // 10
+        Z=13
+        Y=147
+        for y in range(7):
+            Y-=1
+            Z=13
+            for x in range(5):
+                Z+=1
+                if LCD_font_styles[int(code*7+y)][x] == "1":
+                    BLOCK= BLOCKON
+                    mc.setBlock(47, Y,Z,BLOCK)   
+                else:
+                    BLOCK= BLOCKOFF
+                    mc.setBlock(47, Y,Z,BLOCK)   
+                # 桁の原点
+                # ドットの原点座標
+                
+                # ドットを描く
+        code=10
+        Z=7
+        Y=147
+        for y in range(7):
+            Y-=1
+            Z=7
+            for x in range(5):
+                Z+=1
+                if LCD_font_styles[int(code*7+y)][x] == "1":
+                    BLOCK= BLOCKON
+                    mc.setBlock(47, Y,Z,BLOCK)   
+                else:
+                    BLOCK= BLOCKOFF
+                    mc.setBlock(47, Y,Z,BLOCK)      
+# 分 
+        code=dt_now.minute % 10
+        Z=1
+        Y=147
+        for y in range(7):
+            Y-=1
+            Z=1
+            for x in range(5):
+                Z+=1
+                if LCD_font_styles[int(code*7+y)][x] == "1":
+                    BLOCK= BLOCKON
+                    mc.setBlock(47, Y,Z,BLOCK)   
+                else:
+                    BLOCK= BLOCKOFF
+                    mc.setBlock(47, Y,Z,BLOCK)
+                    
+                # 桁の原点
+                # ドットの原点座標
+                
+                # ドットを描く
+        code=dt_now.minute // 10  
+        Z=-5
+        Y=147
+        for y in range(7):
+            Y-=1
+            Z=-5
+            for x in range(5):
+                Z+=1
+                if LCD_font_styles[int(code*7+y)][x] == "1":
+                    BLOCK= BLOCKON
+                    mc.setBlock(47, Y,Z,BLOCK)   
+                else:
+                    BLOCK= BLOCKOFF
+                    mc.setBlock(47, Y,Z,BLOCK)
+                # 桁の原点
+                # ドットの原点座標
+                
+                # ドットを描く
+        code=10
+        Z=-11
+        Y=147
+        for y in range(7):
+            Y-=1
+            Z=-11
+            for x in range(5):
+                Z+=1
+                if LCD_font_styles[int(code*7+y)][x] == "1":
+                    BLOCK= BLOCKON
+                    mc.setBlock(47, Y,Z,BLOCK)   
+                else:
+                    BLOCK= BLOCKOFF
+                    mc.setBlock(47, Y,Z,BLOCK)
+                    # 時
+        code=dt_now.hour % 10 
+        Z=-17
+        Y=147
+        for y in range(7):
+            Y-=1
+            Z=-17
+            for x in range(5):
+                Z+=1
+                if LCD_font_styles[int(code*7+y)][x] == "1":
+                    BLOCK= BLOCKON
+                    mc.setBlock(47, Y,Z,BLOCK)   
+                else:
+                    BLOCK= BLOCKOFF
+                    mc.setBlock(47, Y,Z,BLOCK)
+                # 桁の原点
+                # ドットの原点座標
+                
+                # ドットを描く
+        code=dt_now.hour // 10
+        Z=-23
+        Y=147
+        for y in range(7):
+            Y-=1
+            Z=-23
+            for x in range(5):
+                Z+=1
+                if LCD_font_styles[int(code*7+y)][x] == "1":
+                    BLOCK= BLOCKON
+                    mc.setBlock(47, Y,Z,BLOCK)   
+                else:
+                    BLOCK= BLOCKOFF
+                    mc.setBlock(47, Y,Z,BLOCK)
+                # 桁の原点
+                # ドットの原点座標
+                
+                # ドットを描く
+
+                     
+        display2.update_col(col=0, code=int(str(dt_now.year)[0]))
+        display2.update_col(col=1, code=int(str(dt_now.year)[1]))
+        display2.update_col(col=2, code=int(str(dt_now.year)[2]))
+        display2.update_col(col=3, code=int(str(dt_now.year)[3]))
+        display2.update_col(col=4, code=11)
+        display2.update_col(col=5, code=int(str(dt_now.month)[0]))
+        display2.update_col(col=6, code=11)
+        display2.update_col(col=7, code=int(str(dt_now.day)[0]))
+        # display2.update_col(col=8, code=int(str(dt_now.day)[1]))  
+        Z+=1
+        dt_now = datetime.now()
+        time_now = (dt_now.hour * 10000
+                    + dt_now.minute * 100
+                    + dt_now.second)
+        
+
+        pygame.display.flip()  # update_col
+        clock.tick(FPS)  # FPS, Frame Per Second
+    screen.fill(DARK_GRAY)
+# infinit loop bottom ----
---
+++ examples/new_Minecraft.py
@@ -0,0 +1,108 @@
+# demo for 7-segment simulation
+# using the class 'Seven_seg' in seven_seg_pg.py
+# まだ あ～おまでしか作っていない
+import sys
+from datetime import datetime
+import pygame
+import pygame.freetype
+# import time
+from lcd_font_pg_seven_seg import LCD_font
+from mc_remote.minecraft import Minecraft
+import param_mc_remote as param
+from param_mc_remote import PLAYER_ORIGIN as po
+from param_mc_remote import block
+from param_mc_remote import particle
+from param_mc_remote import entity
+import time
+with open("fonts/font.txt", encoding="utf-8") as f:
+        LCD_font_styles = f.read().split('\n')
+disp_msg = "inecraft  Hello World Let's play Minecraft "
+i = 0
+DARK_GRAY = (40, 40, 40)
+GRAY = (80, 80, 80)
+RED = (255, 0, 0)
+GREEN = (10, 250, 10)
+CYAN = (120, 120, 250)
+YELLOW = (250, 250, 20)
+WHITE = (250, 250, 250)
+pygame.init()
+clock = pygame.time.Clock()
+FPS=1
+BLOCKOFF=block.AIR
+mc = Minecraft.create(address=param.ADRS_MCR, port=param.PORT_MCR)
+result = mc.setPlayer(param.PLAYER_NAME, po.x, po.y, po.z)
+if ("Error" in result):
+    sys.exit(result)
+else:
+    print(result)
+lcd1 = LCD_font(mc)
+
+running = True
+while running:
+    for count in range(16 ** 4):  # 0から65535まで
+        # press ctrl-c or close the window to stop
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+           
+        if not running:
+            break
+    Z=45
+    Y=80
+    X=20
+    Yorizin=Y
+    Zorizin=Z
+    Xorizin=X
+    BLOCKON=block.DIRT
+    count=0
+    count_2=len(disp_msg)
+    count_3=0
+    count_4=1    
+    for i in range (len(disp_msg)-8):
+        count_3+=1
+        for i in range (count_2):
+                count+=1
+                for i in range(count):
+                    msg=(ord(disp_msg[i]))
+                code = msg
+                print(ord(disp_msg[i]))
+                Z=Zorizin
+                X=Xorizin
+                Y=Yorizin
+                for y in range(7):
+                    Y-=1
+                    Z=Zorizin
+                    for x in range(5):
+                        Z-=1
+                        if LCD_font_styles[int(code*7+y)][x] == "1":
+                            BLOCK = BLOCKON
+                            mc.setBlock(X, Y,Z,BLOCK)
+                        else :
+                            BLOCK =BLOCKOFF
+                            mc.setBlock(X, Y,Z,BLOCK)
+               
+
+                # 桁の原点
+                # ドットの原点座標
+                
+                # ドットを描く
+                Zorizin-=9
+        Z=45
+        Zorizin=Z
+        time.sleep(0.3)
+        count_2-=1
+        count=0
+        count+=count_3
+        clock.tick(FPS)
+
+    
+    
+                
+
+
+        
+
+# infinit loop bottom ----
+
+
+
